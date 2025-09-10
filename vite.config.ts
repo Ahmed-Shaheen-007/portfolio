2edc689df02998490fc9d1681372b26d0eff6e22
Ahmed Emad Shaheen <ahmedemadshaheen122@gmail.com>
2025-09-10 03:25:05 +0300

    Add production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,41 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
->>>>>>> 83e087f67cb2a3020978dbaaea568e2e63042f49
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { cartographer } from "@replit/vite-plugin-cartographer";
 
-export default defineConfig({
-<<<<<<< HEAD
-  base: "/portfolio/",   // ← هنا حط اسم الريبو بتاعك
-=======
->>>>>>> 83e087f67cb2a3020978dbaaea568e2e63042f49
+export default defineConfig(({ mode }) => ({
+  base: '/',
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
+    ...(mode !== "production" && process.env.REPL_ID !== undefined
+      ? [cartographer()]
       : []),
   ],
+
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+
+  root: path.resolve(__dirname, "client"),
+
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
+
   server: {
     fs: {
       strict: true,
       deny: ["**/.*"],
     },
+    port: 3000,
+    open: true,
   },
-});
+}));
